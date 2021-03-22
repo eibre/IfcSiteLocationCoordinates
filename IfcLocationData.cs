03c4666029859ae2ea0d31d14eaca4b9062202e9
Einar Raknes <Einar.Raknes@norconsult.com>
2021-03-22 22:15:42 +0100

    Add project files.

---
+++ IfcLocationData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xbim.Ifc;
+using Xbim.Common.Collections;
+using Xbim.Ifc2x3.Interfaces;
+
+namespace IfcSiteLocationCoordinates
+{
+    class IfcLocationData
+    {
+        public IfcLocationData(string path)
+        {
+
+            using (IfcStore model = IfcStore.Open(path))
+            {
+                IIfcProject project = model.Instances.FirstOrDefault<IIfcProject>();
+                AuthoringTool = project.OwnerHistory.OwningApplication.ApplicationFullName;
+                
+                IIfcSite site = model.Instances.FirstOrDefault<IIfcSite>();
+                refElevation = site.RefElevation.Value;
+                IIfcLocalPlacement placement = site.ObjectPlacement as IIfcLocalPlacement;
+                IIfcAxis2Placement3D axis2Placement = placement.RelativePlacement as IIfcAxis2Placement3D;
+                double x = axis2Placement.RefDirection.DirectionRatios.GetAt(0);
+                double y = axis2Placement.RefDirection.DirectionRatios.GetAt(1);
+                double angle = 360-Math.Atan2(y, x)*180/Math.PI;
+                Orientation = angle;
+
+                EW = axis2Placement.Location.Coordinates.GetAt(0);
+                NS = axis2Placement.Location.Coordinates.GetAt(1);
+                elevation = axis2Placement.Location.Coordinates.GetAt(2);
+
+            }
+        }
+
+        public double refElevation { get; set; }
+        public double Orientation { get; set; }
+        public double EW { get; set; }
+        public double NS { get; set; }
+        public double elevation { get; set; }
+
+        public string AuthoringTool { get; set; }
+        public string LengthUnit { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IfcSiteLocationCoordinates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void browseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = "Opening " + openFileDialog.FileName + "..."; 
+                
+                IfcLocationData data = new IfcLocationData(openFileDialog.FileName);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("File Path: " + openFileDialog.FileName);
+                sb.Append("\n Authoring program: " + data.AuthoringTool);
+                sb.Append("\n\nIfcSite.RefElevation is: " + data.refElevation.ToString());
+                sb.Append("\n\nInfo from IfcSites IfcLocalPlacement:");
+                sb.Append("\nAngle to true north is: " + data.Orientation.ToString());
+                sb.Append($"\nEW: {data.EW}, NS: {data.NS}");
+                sb.Append("\nElevation: " + data.elevation.ToString());
+
+
+                this.textBox.Text = sb.ToString();
+            }
+        }
+    }
+}
